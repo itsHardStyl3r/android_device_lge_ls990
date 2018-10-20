a7293f05568a687982039f9c72405843469d3042
jrior001 <jriordan001@gmail.com>
2018-10-20 18:35:01 +0200

    ls990: use prebuilt NFC HAL from 15.1
    Change-Id: I03a252ff820d5bd8dde8faac5fa293242aaef1f2

--- device.mk
+++ device.mk
@@ -30,6 +30,9 @@ PRODUCT_PACKAGES += \
     libnfc_jni \
     Nfc
 
+PRODUCT_COPY_FILES += \
+    vendor/lge/g3-common/proprietary/vendor/lib/hw/nfc_nci.msm8974.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/nfc_nci.msm8974.so
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
