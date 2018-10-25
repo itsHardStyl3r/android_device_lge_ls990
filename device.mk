aaaac124f6c5858ac724c2a6889dd31370225caf
dianlujitao <dianlujitao@lineageos.org>
2018-10-25 14:37:32 +0200

    ls990: use TARGET_COPY_OUT_VENDOR
    Change-Id: Idc3c93a8531cd95334655bddb3e3f0d647311ed4

--- device.mk
+++ device.mk
@@ -17,8 +17,8 @@
 
 # Audio
 PRODUCT_COPY_FILES += \
-    device/lge/g3-common/configs/audio/audio_platform_info_bcm.xml:system/vendor/etc/audio_platform_info.xml \
-    device/lge/g3-common/configs/audio/mixer_paths_bcm.xml:system/vendor/etc/mixer_paths.xml
+    device/lge/g3-common/configs/audio/audio_platform_info_bcm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    device/lge/g3-common/configs/audio/mixer_paths_bcm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # GPS
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
